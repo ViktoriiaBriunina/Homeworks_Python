6b0b78c86f68067ea91cb114e25608530b1f6840
Viktoriia B <viktoriia.briunina@gmail.com>
2023-03-05 17:13:22 +0200

    my commit_3_2

---
+++ homework_3_2.py
@@ -0,0 +1,7 @@
+age = int(input("Enter your age please:"))
+name = input("Enter your name please")
+ageMin = 21
+if age >= ageMin:
+    print("Welcome to the club" + " " + name + "!")
+else:
+    print("Sorry" + " " + name + "," + "you are too young")
