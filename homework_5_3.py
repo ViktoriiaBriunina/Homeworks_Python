501ff850c8bbd8047d1c0d8f4ef977e270de3cf5
Viktoriia B <viktoriia.briunina@gmail.com>
2023-03-11 17:01:48 +0200

    commit_5_3

---
+++ homework_5_3.py
@@ -0,0 +1,11 @@
+def fact(x):
+    """Calculate a Factorial for number X"""
+    answer = 1
+    for i in range(1, x + 1):
+        answer = answer * i
+    return answer
+
+print(fact(5))
+
+for i in range (1,7):
+    print(fact(i))
